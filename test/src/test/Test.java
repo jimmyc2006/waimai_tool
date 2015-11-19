584d8cf224f596d05ad7e5814643ea99898516d7
shuwei <shuwei003@163.com>
2015-11-19 09:04:53 +0800

    提交

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -1,5 +1,10 @@
 package test;
 
-public class Test {
+import java.text.SimpleDateFormat;
 
+public class Test {
+	public static void main(String[] args) {
+		SimpleDateFormat sdf = new SimpleDateFormat();
+		System.out.println("123".split("\\01"));
+	}
 }
